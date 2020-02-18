bfbfe7751b54e0853902278daaceed8c001ef01d
Jason <JSilvis030592@yahoo.com>
2020-02-18 14:29:18 -0500

    Finish Project

--- EFCoreTutorial/SongRepository.cs
+++ EFCoreTutorial/SongRepository.cs
@@ -6,39 +6,12 @@ using System.Text;
 
 namespace EFCoreTutorial
 {
-  public class SongRepository
+    public class SongRepository : Repository<Song>
     {
-        private MusicContext db;
-
-        public SongRepository(MusicContext db)
+            public SongRepository(MusicContext context) : base(context)
         {
-            this.db = db;
-        }
-
-        public int Count()
-        {
-            return db.Songs.Count();
-        }
-
-        public void Create(Song song)
-        {
-            db.Songs.Add(song);
-            db.SaveChanges();
-        }
-
-        public object GetById(int id)
-        {
-            return db.Songs.Single(s => s.Id == id);
-        }
-
-        public void Delete(Song song)
-        {
-            throw new NotImplementedException();
-        }
-
-        public object GetAll()
-        {
-            throw new NotImplementedException();
+            // This ensures our base constructor gets run.
+    
         }
     }
 }
